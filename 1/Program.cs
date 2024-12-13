d4b5358c6e95dbc97676ce75f274e77d4a3e4dab
El hamd <El hamd@DESKTOP-ANPFBI8>
2024-12-13 07:13:09 +0200

    Add project files.

---
+++ 1/Program.cs
@@ -0,0 +1,115 @@
+﻿namespace _1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int x = 5;
+            Console.WriteLine(x);
+            //variables ==> location stored in memory to store data(name, datatype, value, size, address)
+            //alocate ==> store space in memory
+            //size ==> 2^number of bites
+            int y = 5;
+            Console.WriteLine(y);
+            //solution ==> projects ==> classes ==> function 
+            //main function is entry point and mus be one entry point in project    
+            //i can write any language in .net but must have same cls
+            /*memory(RAM)
+             1- stack
+                    a. store any data with fixed size like int 
+                    b. store value type data
+                    c. speed of accesbility in stack is fast than heap, cause stack is more arranged than heap
+             2- heap
+                    a. store reference type
+                    b. store data with dynamic size
+                    c. it can be exbaned until memory(ram) is full and then program crash
+                       but before crash, something called garbage collector try to remove all un unsed space in heap
+                    d. size of heap is bigger stack 
+             */
+            #region newwithclr
+            /*
+              point = new point(), so with write this NEW
+                 ==> allocate or store number of bytes in heap for object
+                 ==> intiliaze the allocated bytes in heap with default value of datatype
+                 ==> call user_defined constructor
+                 ==> assign in reference in stack  the address of object which created in heap
+              */
+            #endregion
+
+            /* casting
+                implict casting (safe casting)
+                    ==> from int to double
+                explicit casting (un safe casting)
+                    ==> it will change the shape of data
+                    double x = 1.2;
+                    int y = (int) x;
+             */
+            #region exincasting
+            double x2 = 5.2;
+            Console.WriteLine(x2);
+            int y2 = (int)x2;
+            Console.WriteLine(y2);
+            long x3 = 111111111111111111;
+            int y3 = (int)x3;
+            Console.WriteLine(y3); //==> change shape of data 
+            #endregion
+
+            #region parse(unsafe) cause it can throw exception and make program stop
+            //parse :function ==> exist in all classes and struct of datatype
+            //int g = int.Parse(Console.ReadLine());
+            //Console.WriteLine(g);
+            #endregion
+
+            //CONVERt is class (unsafe) cause it can throw exception and make program stop
+            //int g0 = Convert.ToInt32(Console.ReadLine());
+
+            //try parse: function (safe) cause if sucees it return true and assign value to second paramter but if false return zero to assign number
+            bool test = int.TryParse(Console.ReadLine(), out x);
+                        Console.WriteLine(test);
+                        Console.WriteLine(x);
+
+            //bydefaut any fraction is double so if i want to write fraction with decimal or float need to add (m , f)
+            double x4 = 1.5;
+            float y4 = 1.5f;
+            Console.WriteLine(y4);
+            decimal x5 = 1.5m;
+            Console.WriteLine(x5);
+
+            #region operator
+            /*
+             1- unary increment and decrement (++,--) work on one operand like variable
+                        ++x ==> first increment then do what is next (prefix)
+                        x++ ==> first do what is before and then increment
+            2- binary operator work with two operand (+, -, *, /, %)
+            3- assigned operator ==> x += 5 ==> x = x + 5
+            4- relational operator (==, !=, >, <, >=, <=) used with comparsion  and return true or false
+            5- logical operator !(reverse)
+                                && (and) ==> short circuit like if first is false so result will be false
+                                || (or) ==> if one is true result will be true and it short circuit
+            6- bitwise operator ==> same as logical operator but long circuit like in (and&&) if first is false, it sill continue and xhexk second condition
+            7- ternary operator ==> relational operator(return true or false) ? (here if return true) : (if return false)
+                               
+             */
+
+            //int y5 = 5;
+            //Console.WriteLine(++y5); //like here increment then print
+            //Console.WriteLine(y5++); //like here print then increment
+            //Console.WriteLine(y5);
+
+            //int sum, n01 = 1, n02 = 2;
+                //sum = n01 + n02;
+            //Console.WriteLine(sum);
+            #endregion
+
+
+
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ assigment/Program.cs
@@ -0,0 +1,129 @@
+﻿namespace assigment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1
+            //Write a program that allows the user to enter a number then print it.
+            //Console.WriteLine("ENTER NUMBER");
+            //int NUMBER;
+            //int.TryParse(Console.ReadLine(), out NUMBER);
+            //Console.WriteLine($"number u entered: {NUMBER}");
+            #endregion
+
+            #region 2
+            string non_numerical = "osama";
+            int Return;
+            bool test_convert = int.TryParse(non_numerical, out Return);
+            //Console.WriteLine($"result will be {test_convert} then integar value: {Return}");
+            //Console.WriteLine(test_convert? $"can converted so value will be: {Return}" : $"cant converted so value will be: {Return}");
+            #endregion
+
+            #region 3
+            float test = 1.4f;
+            //Console.WriteLine(++test);
+            //Console.WriteLine(test + 1.5);
+            #endregion
+
+            #region 4
+            string test2 = "osama elsafty";
+            string Result1 = test2.Substring(0, 5); //take paramter (start point(0), length of new substring(5)) 
+            string Result2 = test2.Substring(5); //start point 5 to the end
+                                                 //Console.WriteLine(Result1);
+                                                 //Console.WriteLine(Result2);
+            #endregion
+
+            #region 6
+            int var1 = 5, var2;
+            var2 = var1;
+            //Console.WriteLine($"when assign var1 to var2,so value of 2 will be: {var2}");
+            var1 += 5;
+            //Console.WriteLine($"after modifiy var1 so var2 = {var2}");
+            var2 += 5;
+            //Console.WriteLine($"after modifiy var2 so var2 = {var2}");
+            #endregion
+
+            #region 7
+            string first = "osama", second = " elsafty";
+            //Console.WriteLine(first + second);
+            //Console.WriteLine($"{first}{second}");
+            #endregion
+
+            #region 8
+            //double principal, rate, time;
+            //Console.WriteLine("enter principal then rate then time");
+            //double.TryParse(Console.ReadLine(), out principal);
+            //double.TryParse(Console.ReadLine(), out rate);
+            //double.TryParse(Console.ReadLine(), out time);
+            //double Interest = (principal * rate * time) / 100;
+            //Console.WriteLine(Interest);
+            #endregion
+
+            #region 9
+            //double weight, height;
+            // Console.WriteLine("enter weight");
+            //double.TryParse(Console.ReadLine(), out weight);
+            //Console.WriteLine("enter height");
+            //double.TryParse(Console.ReadLine(), out height);
+            //double BMI = (weight) / (height * height);
+            //Console.WriteLine(BMI);
+            #endregion
+
+            #region 10
+            //double temp;
+            //double.TryParse(Console.ReadLine(), out temp);
+            //Console.WriteLine(temp < 10 ? "TOO COLD" : temp > 30 ? "TOO HOT" : "is good");
+            #endregion
+
+            #region 11
+            /*
+            int day, month, year;
+            Console.WriteLine("ENTER DAY");
+            int.TryParse(Console.ReadLine(), out day);
+            Console.WriteLine("ENTER MONTH");
+            int.TryParse(Console.ReadLine(), out month);
+            Console.WriteLine("ENTER YEAR");
+            int.TryParse(Console.ReadLine(), out year);
+
+            if ((day < 32 && day > 0) && (month < 13 && month > 0) && (year < 9999 && year >= 2001))
+            {
+                Console.WriteLine($"{day} , {month} , {year}\n{day} / {month} / {year}\n{day} - {month} - {year}");
+            }
+            else
+            {
+                Console.WriteLine($"u enter not correct data");
+            }
+            */
+            #endregion
+
+            #region 12
+            /*
+             What is the output of the following C# code?
+             DateTime date = new DateTime(2024, 6, 14); Console.WriteLine($"The event is on {date:MM/dd/yyyy}");
+              The event is on 06/14/2024
+            */
+            #endregion
+
+            #region 13
+            /*
+             Which of the following statements is correct about the C#.NET code snippet given below?
+             int d; 
+            d = Convert.ToInt32( !(30 < 20) ); //output here is true = 1
+            D = 1 //cause when convert from bool to int true will be 1 and false 0
+            */
+            #endregion
+
+            #region 14
+            //6 1
+            #endregion
+
+            #region 15
+            //first condition(2 + 5 + '' + 7) = 7 7
+            #endregion
+
+        }
+
+    }
+}
+    
